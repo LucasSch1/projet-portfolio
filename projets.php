fa777afa4b6885bf5aaef707bedf38acb0b4761b
Lucas SCHNELZAUER <lucas.schnelzauer@outlook.fr>
2024-04-16 10:27:10 +0200

    Ajout des projets sur la page Projets

--- projets.php
+++ projets.php
@@ -4,19 +4,43 @@
 <html lang="en">
 <body>
     <h1 class="text-center text-white">Mes projets</h1>
-    <div class="liste d-flex justify-content-center">
+    <div class="liste d-flex justify-content-center flex-column">
+        <div class="container my-3">
+            <div class="row projet1">
+                <div class="col-md-6 p-3">
+                <img src="/images/images_projets/jeu_de_balles/jeu_balles_presentation.png" alt="Image du projet" class="img-fluid w-50 justify-content-center" >
+                </div>
+                <div class="col-md-6 p-3">
+                <h2>Jeu de balles</h2>
+                <p>Projet réalisé durant le challenge de 1ère année de BTS SIO , consistant à réaliser un jeu permettant de cliquer sur les balles demandées     afin de gagner des points . </p>
+                <a href="#">En savoir plus</a>
+                </div>
+            </div>
+        </div>
+        <div class="container mb-3">
+            <div class="row projet1">
+                <div class="col-md-6 p-3">
+                <img src="/images/images_projets/jeu_des_pays/jeu_des_pays_presentation.png" alt="Image du projet" class="img-fluid w-50 justify-content-center" >
+                </div>
+                <div class="col-md-6 p-3">
+                <h2>Jeu des Pays</h2>
+                <p>TP réalisé en 1ère année de BTS SIO, consistant à deviner les différents pays se finissant par -stan.</p>
+                <a href="#">En savoir plus</a>
+                </div>
+            </div>
+        </div>
         <div class="container">
-    <div class="row">
-        <div class="col-md-6">
-        <img src="/images/projet_image.png" alt="Image du projet" class="img-fluid w-50 justify-content-center" >
+            <div class="row projet1">
+                <div class="col-md-6 p-3">
+                <img src="/images/images_projets/tp_php/tp_menu_restaurant_presentation.png" alt="Image du projet" class="img-fluid w-50 justify-content-center" >
+                </div>
+                <div class="col-md-6 p-3">
+                <h2>TP menu restaurant</h2>
+                <p>TP réalisé en 2ème année de BTS SIO, consistant à appliquer le cour concernant le langage de programmation PHP.</p>
+                <a href="#">En savoir plus</a>
+                </div>
+            </div>
         </div>
-        <div class="col-md-6">
-        <h2>Titre du projet</h2>
-        <p>Description du projet.</p>
-        <a href="#">En savoir plus</a>
-        </div>
-    </div>
-    </div>
     </div>
 </body>
 </html>
